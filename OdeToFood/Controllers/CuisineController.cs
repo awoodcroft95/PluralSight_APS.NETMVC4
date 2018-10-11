b1fa34e9176aa929453e88536a7427f7dd6daf5b
awoodcroft95 <awoodcroft95@gmail.com>
2018-10-11 16:08:30 +0100

    First Commit of Project Files

---
+++ OdeToFood/Controllers/CuisineController.cs
@@ -0,0 +1,21 @@
+﻿using OdeToFood.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OdeToFood.Controllers
+{
+    //[Authorize]
+    [Log]
+    public class CuisineController : Controller
+    {
+        public ActionResult Search(string name)
+        {
+            throw new Exception("An error ocurred!");
+            var message = Server.HtmlEncode(name);
+            return Content(message);
+        }
+    }
+}
